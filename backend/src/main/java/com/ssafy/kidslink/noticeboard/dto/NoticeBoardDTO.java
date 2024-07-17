d0cbbd39587ba5718952497aef246ea3304f9e4c
김지원 <gokim0928@naver.com>
2024-07-17 17:48:10 +0900

    feat: 알림장 조회 기능 초기 구성
    - kindergarten domain
    - kindergartenclass domain
    - noticebaord domain

---
+++ backend/src/main/java/com/ssafy/kidslink/noticeboard/dto/NoticeBoardDTO.java
@@ -0,0 +1,13 @@
+package com.ssafy.kidslink.noticeboard.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class NoticeBoardDTO {
+    private String title;
+
+}
