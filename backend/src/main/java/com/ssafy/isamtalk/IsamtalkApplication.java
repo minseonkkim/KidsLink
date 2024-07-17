f136cd0659f44a8be10cb4c1a247f2126a97035c
Kimbumsoo99 <show7441@naver.com>
2024-07-17 13:29:04 +0900

    feat: Backend 프로젝트 init
    - 초기 세팅 start.spring.io를 통해 추가
    - 관련 내용은 팀 노션 version 부분 작성

---
+++ backend/src/main/java/com/ssafy/isamtalk/IsamtalkApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.isamtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsamtalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsamtalkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/isamtalk/IsamtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.isamtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsamtalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
