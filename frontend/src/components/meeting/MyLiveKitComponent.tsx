ab0728854eb960dd8e464fc7156ad552553be5bf
김여준 <kt65346337@gmail.com>
2024-07-18 17:52:16 +0900

    feat: 화상채팅 기능 삽입
    - 화상채팅 코드 삽입
    
    fixes: #230
    related to: #199, #197

---
+++ frontend/src/components/meeting/MyLiveKitComponent.tsx
@@ -0,0 +1,26 @@
+import { AudioConference, CameraDisabledIcon, ControlBar, DisconnectButton, LiveKitRoom, RoomAudioRenderer } from '@livekit/components-react';
+import * as React from 'react';
+
+const MyLiveKit = () => {
+  const serverUrl = '';
+  const accessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjExODQ0MTMsImlzcyI6IkFQSUVvOUpFZFlSS1l0USIsIm5iZiI6MTcyMTE4MzUxMywic3ViIjoiaXNhbXRhbGsiLCJ2aWRlbyI6eyJjYW5QdWJsaXNoIjp0cnVlLCJjYW5QdWJsaXNoRGF0YSI6dHJ1ZSwiY2FuU3Vic2NyaWJlIjp0cnVlLCJyb29tIjoiaXNhbXRhbGsiLCJyb29tSm9pbiI6dHJ1ZX19.7MToMgOA2U8H0T05VYunUEuBKVXrAb9ZPtUjdAVT6p4';
+  
+  const controlBarControls = {
+    microphone: true,
+    camera: true,
+    chat: true,
+    screenShare: true,
+    leave: true,
+    settings: true,
+  };
+
+  return (
+    <div>
+      <LiveKitRoom serverUrl={serverUrl} token={accessToken} connect={true}>
+        <ControlBar controls={controlBarControls}/>
+      </LiveKitRoom>
+    </div>
+  );
+};
+
+export default MyLiveKit;
