6d570be92a9597831e10ccc413aed0aee8dd0cac
minseonkkim <minsun9856@gmail.com>
2024-07-18 11:23:56 +0900

    상담가능시간 open 화면 구현
    - 캘린더 커스터마이징
    - 시간 컴포넌트 구현

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -16,7 +16,7 @@ export default function Login(){
                     <input type="text" placeholder="로그인" className="w-[450px] h-[62px] rounded-[20px] p-5 bg-white border border-[#b9b9b9] mb-3 text-[22px] font-medium text-left text-[#b9b9b9]"/>
                     <input type="password" placeholder="비밀번호" className="w-[450px] h-[62px] rounded-[20px] p-5 bg-white border border-[#b9b9b9] mb-5 text-[22px] font-medium text-left text-[#b9b9b9]"/>
                     <button className="w-[450px] h-[62px] rounded-[20px] bg-[#ffe96f] text-[24px] font-bold mb-3">로그인</button>
-                    <p className="text-[22px] font-medium text-left text-[#363636]">아이샘톡이 처음이라면?</p>
+                    <p className="text-[22px] font-medium text-left text-[#363636]">키즈링크가 처음이라면?</p>
                 </form>
             </div>
 
--- frontend/src/pages/teacher/TeacherReservation.tsx
+++ frontend/src/pages/teacher/TeacherReservation.tsx
@@ -1,10 +1,74 @@
+import NavigateBack from "../../components/teacher/common/NavigateBack";
 import TeacherHeader from "../../components/teacher/common/TeacherHeader";
+import Title from "../../components/teacher/common/Title";
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import moment from "moment";
+import { FaRegCalendar } from "react-icons/fa6";
+import ReservationTime from "../../components/teacher/consulting/ReservationTime";
 
-export default function TeacherReservation(){
-    return <>
-      <TeacherHeader/>
-      <div className="font-KoPubDotum px-[150px]">
-          
-      </div>
-    </>
+export default function TeacherReservation() {
+    type ValuePiece = Date | null;
+    const [date, setDate] = useState<ValuePiece | [ValuePiece, ValuePiece]>(new Date());
+
+    const formatDate = (date: ValuePiece | [ValuePiece, ValuePiece]) => {
+        if (date instanceof Date) {
+            return moment(date).format("YYYY년 MM월 DD일");
+        } else if (Array.isArray(date) && date[0] instanceof Date) {
+            return moment(date[0]).format("YYYY년 MM월 DD일");
+        }
+        return '';
+    };
+
+    return (
+        <>
+            <TeacherHeader />
+            <div className="font-KoPubDotum px-[150px]">
+                <NavigateBack backPage="화상상담" backLink='/meeting' />
+                <Title title="상담가능시간 open" />
+                <div className="flex flex-row justify-between">
+                    <Calendar onChange={setDate} value={date} formatDay={(locale, date) => date.toLocaleString("en", {day: "numeric"})}/>
+                    <div className="w-[637px]">
+                        <div className="flex flex-row justify-between">
+                            <div className="text-[22px] flex flex-row items-center h-[22px] font-bold text-[#8CAD1E] my-5">
+                                <FaRegCalendar className="mr-3"/>
+                                {formatDate(date)}
+                            </div>
+                            <button className="h-[40px] border-[2px] border-[#7C7C7C] bg-[#E3EEFF] px-3 py-1 font-bold rounded-[10px] hover:bg-[#D4DDEA]">수정하기</button>
+                        </div>
+                        <p className="mb-3 font-bold text-[18px]">오전</p>
+                        <div className="flex flex-row flex-wrap">
+                            <ReservationTime time="9:00" isActive={false}/>
+                            <ReservationTime time="9:30" isActive={true}/>
+                            <ReservationTime time="10:00" isActive={false}/>
+                            <ReservationTime time="10:30" isActive={false}/>
+                            <ReservationTime time="11:00" isActive={false}/>
+                            <ReservationTime time="11:30" isActive={false}/>
+                        </div>
+                        <p className="mt-5 mb-3 font-bold text-[18px]">오후</p>
+                        <div className="flex flex-row flex-wrap">
+                            <ReservationTime time="12:00" isActive={false}/>
+                            <ReservationTime time="12:30" isActive={true}/>
+                            <ReservationTime time="13:00" isActive={true}/>
+                            <ReservationTime time="13:30" isActive={true}/>
+                            <ReservationTime time="14:00" isActive={true}/>
+                            <ReservationTime time="14:30" isActive={true}/>
+                            <ReservationTime time="15:00" isActive={true}/>
+                            <ReservationTime time="15:30" isActive={true}/>
+                            <ReservationTime time="16:00" isActive={true}/>
+                            <ReservationTime time="16:30" isActive={false}/>
+                            <ReservationTime time="17:00" isActive={false}/>
+                            <ReservationTime time="17:30" isActive={false}/>
+                            <ReservationTime time="18:30" isActive={false}/>
+                            <ReservationTime time="18:30" isActive={false}/>
+                            <ReservationTime time="19:00" isActive={false}/>
+                            <ReservationTime time="19:30" isActive={false}/>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </>
+    );
 }
