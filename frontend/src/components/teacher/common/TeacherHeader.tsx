8132e5bee7c78b2a20353590ff30b4cabbdf2ab7
minseonkkim <minsun9856@gmail.com>
2024-07-23 08:52:42 +0900

    S11P12C206-666 feat: 학부모 헤더 반응형 구현

--- frontend/src/components/teacher/common/TeacherHeader.tsx
+++ frontend/src/components/teacher/common/TeacherHeader.tsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 export default function TeacherHeader(){
     return <>
         <div className="flex items-center justify-between h-[85px] bg-[#ffffff] relative shadow-md z-10">
-            <Link to='/'><p className="ml-[150px] text-[40px] font-bold text-left font-Cafe24Ssurround gradient-text cursor-pointer">키즈링크</p></Link>
+            <Link to='/'><p className="max-sm:ml-[30px] ml-[150px] text-[40px] font-bold text-left font-Cafe24Ssurround gradient-text cursor-pointer">키즈링크</p></Link>
             <div className="flex flex-row">
                 <CgProfile className="w-[30px] h-[30px] mr-8 cursor-pointer" style={{ color: '#363636' }} />
-                <div className="relative mr-[150px]">
+                <div className="relative max-sm:mr-[30px] mr-[150px]">
                     <BiBell className="w-[30px] h-[30px] cursor-pointer" style={{ color: '#363636' }} />
                     <span className="absolute top-0 right-0 w-4 h-4 bg-red-500 text-white text-[9px] font-bold rounded-full flex items-center justify-center">2</span>
                 </div>
