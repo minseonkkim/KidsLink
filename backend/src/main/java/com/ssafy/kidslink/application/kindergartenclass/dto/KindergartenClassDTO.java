50ccdbdbbe29f8fe5bb104af9f6e0d43150cf2ea
Kimbumsoo99 <show7441@naver.com>
2024-07-23 15:15:37 +0900

    S11P12C206-475 feat: 유치원 반 조회 API 개발

---
+++ backend/src/main/java/com/ssafy/kidslink/application/kindergartenclass/dto/KindergartenClassDTO.java
@@ -0,0 +1,15 @@
+package com.ssafy.kidslink.application.kindergartenclass.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class KindergartenClassDTO {
+    private Integer kindergartenClassId;
+    private String kindergartenClassName;
+}
