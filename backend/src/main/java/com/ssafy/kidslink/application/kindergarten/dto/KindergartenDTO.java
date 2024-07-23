135a0506866846063f5a5a2a8176fc7303e96764
Kimbumsoo99 <show7441@naver.com>
2024-07-23 14:56:28 +0900

    S11P12C206-474 feat: 유치원 리스트 조회 API 개발

---
+++ backend/src/main/java/com/ssafy/kidslink/application/kindergarten/dto/KindergartenDTO.java
@@ -0,0 +1,2 @@
+package com.ssafy.kidslink.application.kindergarten.dto;public class KindgartenDTO {
+}
---
+++ backend/src/main/java/com/ssafy/kidslink/application/kindergarten/mapper/KindergartenMapper.java
@@ -0,0 +1,2 @@
+package com.ssafy.kidslink.application.kindergarten.mapper;public class KindergartenMapper {
+}
