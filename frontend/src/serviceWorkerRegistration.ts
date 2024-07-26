fbaa18944bd1744003479f3e3e99cadc30b7bccc
sangmin0806 <cjstkd2866@naver.com>
2024-07-26 15:06:31 +0900

    fix:merge-fix

--- frontend/src/serviceWorkerRegistration.ts
+++ frontend/src/serviceWorkerRegistration.ts
@@ -24,6 +24,8 @@ type Config = {
 };
 
 export function register(config?: Config) {
+  console.log("islocalhost:",isLocalhost);
+  console.log("process.env.NODE_ENV:",process.env.NODE_ENV);
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
@@ -36,7 +38,10 @@ export function register(config?: Config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+      const swUrl = isLocalhost
+        ? `${process.env.PUBLIC_URL}/service-worker.js`
+        : `${process.env.PUBLIC_URL}/sw.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
