a9120ca8a17d36d5f7bad460d66a25478bbaa954
김지원 <gokim0928@naver.com>
2024-07-29 10:20:31 +0900

    feat : 알림장 상세 조회 기능 수정 #S11P12c206-486

--- backend/src/main/java/com/ssafy/kidslink/application/noticeboard/mapper/NoticeBoardMapper.java
+++ backend/src/main/java/com/ssafy/kidslink/application/noticeboard/mapper/NoticeBoardMapper.java
@@ -2,15 +2,20 @@ package com.ssafy.kidslink.application.noticeboard.mapper;
 
 import com.ssafy.kidslink.application.noticeboard.domain.NoticeBoard;
 import com.ssafy.kidslink.application.noticeboard.dto.NoticeBoardDTO;
+import com.ssafy.kidslink.application.teacher.repository.TeacherRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class NoticeBoardMapper {
+    private final TeacherRepository teacherRepository;
     public NoticeBoardDTO toDTO(NoticeBoard noticeBoard) {
         NoticeBoardDTO dto = new NoticeBoardDTO();
         dto.setNoticeBoardId(noticeBoard.getNoticeBoardId());
         dto.setContent(noticeBoard.getNoticeBoardContent());
         dto.setTitle(noticeBoard.getNoticeBoardTitle());
+        dto.setTeacherName(teacherRepository.findByKindergartenClass(noticeBoard.getKindergartenClass()).getTeacherName());
         dto.setNoticeBoardDate(noticeBoard.getNoticeBoardDate());
         return dto;
     }
