3f38d588ad8175cf27927f620e2af302cb68fad4
minseonkkim <minsun9856@gmail.com>
2024-07-29 14:47:27 +0900

    feat: 성장일지 반 아이들 리스트 API 연동 #S11P12C206-747
    - 로그인 할 때 선생님 회원 정보 저장
    - 반 아이들 리스트
    - 특정 아이 선택

--- frontend/src/api/notice.ts
+++ frontend/src/api/notice.ts
@@ -5,7 +5,7 @@ interface Notice {
   teacherName: string;
   title: string;
   content: string;
-  noticeBaordDate: string; // 데이터 오타로 옴
+  noticeBoardDate: string; // 데이터 오타로 옴
 }
 
 interface NoticeDetail {
@@ -13,7 +13,7 @@ interface NoticeDetail {
   teacherName: string;
   title: string;
   content: string;
-  noticeBaordDate: string; 
+  noticeBoardDate: string; 
 }
 
 interface NoticeData {
