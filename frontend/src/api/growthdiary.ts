839666acbd71a23fd1dbfa0bacf68facf2e1595c
minseonkkim <minsun9856@gmail.com>
2024-07-30 12:11:38 +0900

    fix: 모달 문제 해결 #S11P12C206-563

--- frontend/src/api/growthdiary.ts
+++ frontend/src/api/growthdiary.ts
@@ -7,40 +7,59 @@ interface Diary{
     images: string[];
 }
 
-
-// 아이별 성장일지 목록 조회
-export async function getChildGrowthDiarys(childId: number): Promise<Diary[]>{
-    try{
-        const response = await axiosInstance.get(`diary/child/${childId}`);
-
-// 특정 아이의 모든 성장일지 조회
-export async function getKidAllGrowthDiarys(childId: number) {
-    try {
-      const response = await axiosInstance.get(`diary/child/${childId}`)
-  
-      if (response.data.status === 'success') {
-        console.log(response.data.data) // 확인 후 삭제
-        return response.data.data
-      } else {
-        throw new Error('Failed to get growth-diary')
-      }
-    } catch (error) {
-      console.error(error)
-      throw error
-    }
+interface DiaryData{
+    diaryDate: string;
+    files: string[];
+    content: string;
 }
 
-// // 성장일지 상세보기 조회
-// export async function getDetailGrowthDiary(diaryId: number){
-//     try{
-//         const response = await axiosInstance.get(`diary/${diaryId}`);
 
-//         if(response.data.status === 'success'){
-//             return response.data
-//         } else{
-//             throw new Error('Failed to get diary')
-//         }
-//     } catch(error){
-//         throw error
-//     }
-// }
+// 아이별 성장일지 목록 조회
+export async function getKidAllGrowthDiarys(childId: number) {
+  try {
+    const response = await axiosInstance.get(`diary/child/${childId}`)
+
+    if (response.data.status === 'success') {
+      console.log(response.data.data) // 확인 후 삭제
+      return response.data.data
+    } else {
+      throw new Error('Failed to get growth-diary')
+    }
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+// 성장일지 상세 조회
+export async function getGrowthDiary(diaryId: number) {
+  try  {
+    const response = await axiosInstance.get(`diary/${diaryId}`)
+
+    if (response.data.status === 'success') {
+      console.log(response.data.data) // 확인 후 삭제
+      return response.data.data
+    } else {
+      throw new Error('Failed to get growth-diary')
+    }
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+// 성장일지 작성
+export async function createDiary(childId:number, diary: DiaryData){
+  try{
+    const response = await axiosInstance.post(`diary/child/${childId}`);
+
+    if(response.data.status === "success"){
+      console.log("create-diary success");
+    } else{
+      throw new Error("cereate-diary failed: " + response.data.message);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
