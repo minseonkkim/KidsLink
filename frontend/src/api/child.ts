9704dd09b9db60db00e6b063462b22e330d72feb
minseonkkim <minsun9856@gmail.com>
2024-07-31 09:22:55 +0900

    feat: 문서 상세 조회 #S11P12C206-757

---
+++ frontend/src/api/child.ts
@@ -0,0 +1,26 @@
+import axiosInstance from "./token/axiosInstance";
+
+interface ChildInfo{
+    name: string;
+    kindergartenClassName: string;
+    kindergartenName: string;
+    gender: string;
+    birth: string;
+    profile: string;
+}
+
+// 아이 id로 아이 정보 조회
+export async function getChildInfo(childId: number){
+    try{
+        const response = await axiosInstance.get(`child/${childId}`);
+    
+        if(response.data.status === 'success'){
+            return response.data.data
+        } else {
+            throw new Error('Failed to get child info')
+        }
+    } catch (error) {
+        console.error(error)
+        throw error
+    }
+}
