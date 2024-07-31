53cb0b894fce765b5d12fbfc77e18b383315558e
Kimbumsoo99 <show7441@naver.com>
2024-07-31 14:40:14 +0900

    feat: 날짜별 문서 조회   #S11P12C206-823
    - childId와 date를 활용하여 날짜별 문서 조회 API 개발

--- backend/src/main/java/com/ssafy/kidslink/application/child/controller/ChildController.java
+++ backend/src/main/java/com/ssafy/kidslink/application/child/controller/ChildController.java
@@ -1,6 +1,7 @@
 package com.ssafy.kidslink.application.child.controller;
 
 import com.ssafy.kidslink.application.child.dto.ChildDTO;
+import com.ssafy.kidslink.application.child.dto.DocumentCheckResponse;
 import com.ssafy.kidslink.application.child.service.ChildService;
 import com.ssafy.kidslink.common.dto.APIResponse;
 import com.ssafy.kidslink.common.exception.InvalidPrincipalException;
@@ -10,10 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/child")
@@ -24,7 +22,7 @@ public class ChildController {
     private final ChildService childService;
 
     @GetMapping("/{childId}")
-    public ResponseEntity<APIResponse<ChildDTO>> detailProcess(@AuthenticationPrincipal Object principal,@PathVariable int childId) {
+    public ResponseEntity<APIResponse<ChildDTO>> detailProcess(@AuthenticationPrincipal Object principal, @PathVariable int childId) {
         if (principal instanceof CustomUserDetails userDetails) {
             ChildDTO childDTO = childService.getChildInfo(childId);
             APIResponse<ChildDTO> responseData = new APIResponse<>(
@@ -38,4 +36,18 @@ public class ChildController {
         }
         throw new InvalidPrincipalException("Invalid user principal");
     }
+
+    @GetMapping("/{childId}/document/check")
+    public ResponseEntity<APIResponse<DocumentCheckResponse>> checkDocument(@PathVariable("childId") Integer childId,
+                                                                            @RequestParam String date) {
+        DocumentCheckResponse documentCheckResponse = childService.checkDocument(childId, date);
+        APIResponse<DocumentCheckResponse> responseData = new APIResponse<>(
+                "success",
+                documentCheckResponse,
+                "아이 문서 여부 조회 성공",
+                null
+        );
+        return ResponseEntity.status(HttpStatus.OK).body(responseData);
+    }
+
 }
