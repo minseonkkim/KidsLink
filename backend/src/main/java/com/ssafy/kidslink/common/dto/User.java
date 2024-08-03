ebcf46fb1e4bfe678b9f59390bb57bcdecd4b208
Kimbumsoo99 <show7441@naver.com>
2024-08-03 19:41:17 +0900

    feat: kakao social login add #S11P12C206-391
    - BCryptPasswordEncoder 주입 시점 SecuritConfig -> AppConfig 변경
    - 카카오 로그인 위한 환경변수 등록 필요

--- backend/src/main/java/com/ssafy/kidslink/common/dto/User.java
+++ backend/src/main/java/com/ssafy/kidslink/common/dto/User.java
@@ -2,9 +2,11 @@ package com.ssafy.kidslink.common.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
+@ToString
 public class User {
     private String username;
     private String name;
