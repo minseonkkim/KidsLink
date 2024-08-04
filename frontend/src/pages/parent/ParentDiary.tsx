0293394591d05fa5f18979d6791397caeed09570
surina125 <jhyunsoo1125@gmail.com>
2024-08-05 06:28:36 +0900

    refactor: 컴포넌트 분리

--- frontend/src/pages/parent/ParentDiary.tsx
+++ frontend/src/pages/parent/ParentDiary.tsx
@@ -1,41 +1,26 @@
-import { useEffect, useRef, useState } from 'react'
-import InfoSection from '../../components/parent/common/InfoSection'
-import BusMap from '../../components/parent/bus/BusMap'
-import busIcon from '../../assets/parent/busIcon.png'
-import { receiveBusLocation } from '../../api/webSocket'
-import { postKidBoardingStatus, getKidBoardingStatus } from '../../api/bus'
-import daramgi from '../../assets/parent/bus-daramgi.png'
-import { useParentInfoStore } from '../../stores/useParentInfoStore'
-import { getParentInfo } from '../../api/Info'
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import InfoSection from "../../components/parent/common/InfoSection"
+import YearMonthSelector from "../../components/parent/common/YearMonthSelector"
+import DiaryList from "../../components/parent/diary/DiaryList"
+import daramgi from "../../assets/parent/growth-daramgi.png"
+import { getKidAllGrowthDiarys, Diary } from "../../api/growthdiary"
+import { useParentInfoStore } from "../../stores/useParentInfoStore"
+import { getParentInfo } from "../../api/Info"
 
-declare global {
-  interface Window {
-    kakao: any;
-  }
-}
-
-export default function ParentBus() {
-  const wsRef = useRef<WebSocket | null>(null)
-  const [location, setLocation] = useState({ lat: 37.5665, lng: 126.9780 })
-  const [isBoarding, setIsBoarding] = useState(false)
-  const [loading, setLoading] = useState(false)
+export default function ParentDiary() {
+  const [diarys, setDiarys] = useState<Diary[]>([])
+  const [filteredDiarys, setFilteredDiarys] = useState<Diary[]>([])
+  const [selectedYear, setSelectedYear] = useState<number>(new Date().getFullYear())
+  const [selectedMonth, setSelectedMonth] = useState<number>(new Date().getMonth() + 1)
+  const navigate = useNavigate()
 
   const parentInfo = useParentInfoStore((state) => state.parentInfo)
   const setParentInfo = useParentInfoStore((state) => state.setParentInfo)
   const childId = parentInfo?.child.childId
 
   useEffect(() => {
-    const cleanup = receiveBusLocation(wsRef, setLocation)
-
-    return () => {
-      if (wsRef.current) {
-        wsRef.current.close()
-      }
-    }
-  }, [])
-
-  useEffect(() => {
-    async function fetchParentInfoAndBoardingStatus() {
+    async function fetchParentInfoAndDiarys() {
       try {
         let currentChildId = childId;
         if (!currentChildId) {
@@ -45,72 +30,62 @@ export default function ParentBus() {
         }
 
         if (currentChildId) {
-          try {
-            const response = await getKidBoardingStatus(currentChildId)
-            setIsBoarding(response.isBoarding)
-          } catch (error) {
-            console.error("Failed to update boarding status", error)
+          const response = await getKidAllGrowthDiarys(currentChildId)
+          if (response) {
+            setDiarys(response)
           }
         }
       } catch (error) {
-        console.error("Failed to fetch parent info", error)
+        console.error("Failed to fetch growth diarys", error)
       }
     }
-
-    fetchParentInfoAndBoardingStatus()
+    fetchParentInfoAndDiarys()
   }, [childId, setParentInfo])
 
-  const handleBoardingStatus = async () => {
-    let currentChildId = childId;
-    if (!currentChildId) {
-      try {
-        const fetchedParentInfo = await getParentInfo()
-        setParentInfo(fetchedParentInfo)
-        currentChildId = fetchedParentInfo.child.childId
-      } catch (error) {
-        console.error("Failed to fetch parent info", error)
-        return
-      }
-    }
+  useEffect(() => {
+    const filtered = diarys.filter(
+      (entry) =>
+        new Date(entry.createDate).getFullYear() === selectedYear &&
+        new Date(entry.createDate).getMonth() + 1 === selectedMonth
+    )
+    setFilteredDiarys(filtered)
+  }, [diarys, selectedYear, selectedMonth])
 
-    if (!currentChildId) {
-      console.error("No childId available")
-      return
-    }
+  const handleYearChange = (increment: number) => {
+    setSelectedYear((prevYear) => prevYear + increment)
+  }
+  const handleMonthChange = (month: number) => {
+    setSelectedMonth(month)
+  }
 
-    setLoading(true)
-    try {
-      const response = await postKidBoardingStatus(currentChildId)
-      setIsBoarding(response.isBoarding)
-    } catch (error) {
-      console.error(error)
-    } finally {
-      setLoading(false)
-    }
+  const handleBoxClick = (diaryId: number) => {
+    navigate(`/diary/${diaryId}`)
   }
 
   return (
     <div className="flex flex-col h-screen bg-[#FFEC8A]">
-      <div className="fixed top-20 right-4 z-50 flex flex-col items-center">
-        <button
-          onClick={handleBoardingStatus}
-          className={`px-4 py-2 rounded-lg ${isBoarding ? 'bg-blue-600 hover:bg-blue-700' : 'bg-orange-400 hover:bg-orange-700'} text-white font-bold transition duration-200`}
-          disabled={loading}
-        >
-          {loading ? 'Loading...' : isBoarding ? '탑승 중' : '탑승하지 않음'}
-        </button>
-      </div>
-
       <InfoSection
-        description1="버스가"
-        main1="이동 중"
-        main2=" 입니다!"
+        description1="교사가 전하는"
+        main1="아이의 성장"
+        main2=" 이야기"
         imageSrc={daramgi}
         altText="다람쥐"
       />
-      
-      <div className="flex-grow w-full bg-white rounded-tl-[20px] rounded-tr-[20px] shadow-top overflow-hidden animate-slideUp -mt-10">
-        <BusMap location={location} busIcon={busIcon} />
+
+      <div className="flex flex-col flex-grow overflow-hidden rounded-tl-[20px] rounded-tr-[20px] bg-white shadow-top px-12 py-4 animate-slideUp -mt-10">
+        <YearMonthSelector
+          selectedYear={selectedYear}
+          selectedMonth={selectedMonth}
+          handleYearChange={handleYearChange}
+          handleMonthChange={handleMonthChange}
+        />
+
+        <div className="flex-grow overflow-y-auto space-y-6 pb-6">
+          <DiaryList
+            filteredDiarys={filteredDiarys}
+            handleBoxClick={handleBoxClick}
+          />
+        </div>
       </div>
     </div>
   )
