7ebaeeef26ecfb272868698cadd4a4ccb35e10fe
Kimbumsoo99 <show7441@naver.com>
2024-08-05 17:34:56 +0900

    fix: Bus 정류장 조회 API 변경  #S11P12C206-646
    - 조회 시 아이 정보도 함께 응답
    - 조회 요청 시 유치원PK 요청 본문 포함 필수

--- backend/src/main/java/com/ssafy/kidslink/application/bus/domain/Bus.java
+++ backend/src/main/java/com/ssafy/kidslink/application/bus/domain/Bus.java
@@ -1,9 +1,12 @@
 package com.ssafy.kidslink.application.bus.domain;
 
+import com.ssafy.kidslink.application.busstop.domain.BusStop;
+import com.ssafy.kidslink.application.kindergarten.domain.Kindergarten;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -11,12 +14,16 @@ import java.time.LocalDateTime;
 public class Bus {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "bus_id")
+    private int busId;
 
-    @Column(name = "latitude")
-    private Double latitude;
-    @Column(name = "longitude")
-    private Double longitude;
-    @Column(name = "timestamp")
-    private LocalDateTime timestamp;
+    @Column(name = "bus_name", nullable = false, length = 50)
+    private String busName;
+
+    @OneToOne
+    @JoinColumn(name = "kindergarten_id", nullable = false)
+    private Kindergarten kindergarten;
+
+    @OneToMany(mappedBy = "bus", cascade = CascadeType.ALL)
+    private Set<BusStop> busStops;
 }
--- backend/src/main/java/com/ssafy/kidslink/application/busstopchild/dto/BusStopChildDTO.java
+++ backend/src/main/java/com/ssafy/kidslink/application/busstopchild/dto/BusStopChildDTO.java
@@ -1,6 +1,7 @@
 package com.ssafy.kidslink.application.busstopchild.dto;
 
 import com.ssafy.kidslink.application.busstopchild.domain.BusStopChild;
+import com.ssafy.kidslink.common.enums.BoardingStatus;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,5 +12,5 @@ import lombok.ToString;
 public class BusStopChildDTO {
     private String childName;
     private String parentTel;
-    private BusStopChild.BoardingStatus status;
+    private BoardingStatus status;
 }
