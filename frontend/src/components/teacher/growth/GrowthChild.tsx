1b5b6b309748a66173ddd446746c4bee4d803ef5
minseonkkim <minsun9856@gmail.com>
2024-08-06 20:32:34 +0900

    fix: 성장일지 작성완료 로직 변경, 오늘 이전 날짜 등록 막음

--- frontend/src/components/teacher/growth/GrowthChild.tsx
+++ frontend/src/components/teacher/growth/GrowthChild.tsx
@@ -13,18 +13,18 @@ export default function GrowthChild({ name, profileImgPath, completed, onClick }
       className="flex flex-col justify-around items-center w-[156px] h-[168px] rounded-[10px] p-2 m-1 font-bold text-[20px] cursor-pointer"
     >
       {!completed ? (
-        <div className="w-[130px] h-[130px]">
+        <div className="w-[120px] h-[120px]">
           <img src={profileImgPath} className="w-full h-full rounded-full object-cover" />
         </div>
       ) : (
-        <div className="relative w-[130px] h-[130px]">
+        <div className="relative w-[120px] h-[120px]">
           <img src={profileImgPath} className="w-full h-full rounded-full object-cover" />
           <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center rounded-full">
             <span className="text-white">작성완료</span>
           </div>
         </div>
       )}
-      <p>{name}</p>
+      <p className="mt-2">{name}</p>
     </div>
   );
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 443,
+    port: 5173,
     host : "0.0.0.0",
   },
   css: {
