f14f1fe50c1656e2a57d5fe18e8ae3626155b868
surina125 <jhyunsoo1125@gmail.com>
2024-08-09 08:20:27 +0900

    design: 학부모 디자인 수정
    - 홈화면 아이이름으로 대체
    - 스케줄 디자인 수정
    - 버스 아이콘 수정
    - 목록 리스트 없을 경우에 문구로 우선 대체(이미지 교체 예정)
    - 성장일지 현재 월 가운데오게 조정, 다른 월 클릭시에도 가운데로 이동되게 조정

--- frontend/src/components/parent/diary/DiaryList.tsx
+++ frontend/src/components/parent/diary/DiaryList.tsx
@@ -9,10 +9,15 @@ interface DiaryListProps {
 export default function DiaryList({ filteredDiarys, handleBoxClick }: DiaryListProps) {
   return (
     <div className="space-y-6 overflow-auto" style={{ maxHeight: 'calc(100vh - 200px)', paddingBottom: '100px', scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
-      {filteredDiarys.map((diary) => (
-        <DiaryItem key={diary.diaryId} diary={diary} handleBoxClick={handleBoxClick} />
-      ))}
+      {filteredDiarys.length > 0 ? (
+        filteredDiarys.map((diary) => (
+          <DiaryItem key={diary.diaryId} diary={diary} handleBoxClick={handleBoxClick} />
+        ))
+      ) : (
+        <p className="text-center text-gray-500 col-span-4">
+          성장일지가 없습니다.
+        </p>
+      )}
     </div>
   )
 }
-
