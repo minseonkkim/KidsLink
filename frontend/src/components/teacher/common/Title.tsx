4a16f5c9716cde309e8aad620ca43b56bcfcd278
minseonkkim <minsun9856@gmail.com>
2024-08-09 14:54:22 +0900

    fix: 주말, 지난 시간 선택 막음 #S11P12C206-592

--- frontend/src/components/teacher/common/Title.tsx
+++ frontend/src/components/teacher/common/Title.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+import { FaInfoCircle } from "react-icons/fa";
+
 interface TitleProps {
     title: string;
+    tooltipContent?: JSX.Element;
 }
 
-export default function Title({title}: TitleProps){
-    return <>
-        <p className="text-[28px] lg:text-[36px] font-bold text-center text-[#363636] mt-0 lg:mt-8 mb-5">
-          {title}
-        </p>
-    </>
+export default function Title({title, tooltipContent}: TitleProps){
+    const [showTooltip, setShowTooltip] = useState<boolean>(false);
+    const [isHovered, setIsHovered] = useState<boolean>(false);
+
+    return (
+        <div className="text-[28px] lg:text-[36px] font-bold text-center text-[#363636] mt-0 lg:mt-8 mb-5 flex flex-row justify-center items-center w-full">
+            {title}
+            <div 
+                className={`relative ml-3 cursor-pointer ${!tooltipContent && 'hidden'}`}
+                onMouseEnter={() => { setShowTooltip(true); setIsHovered(true); }}
+                onMouseLeave={() => { setShowTooltip(false); setIsHovered(false); }}
+            >
+                <FaInfoCircle size={30} color={isHovered ? "#8CAD1E" : "#C0D290"}/>
+                {showTooltip && (
+                  <div className="absolute top-full left-0 mt-2 w-[220px] p-4 bg-white border border-gray-300 rounded shadow-lg z-10 text-sm">
+                      {tooltipContent}
+                  </div>
+                )}
+            </div>
+        </div>
+    );
 }
