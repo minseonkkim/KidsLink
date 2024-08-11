ed1af5d8d06310ce487edd4237864c6b6e156a1f
김여준 <kt65346337@gmail.com>
2024-08-11 15:19:40 +0900

    feat: 화면 전환 시 애니메이션 효과 추가
    - 교사 버스 페이지 적용 안되는 버그 수정해야함

--- frontend/src/pages/teacher/TeacherErrorPage.tsx
+++ frontend/src/pages/teacher/TeacherErrorPage.tsx
@@ -8,11 +8,6 @@ export default function TeacherErrorPage() {
     const navigateToHomePage = () => {
         navigate('/')
     }
-
-    const navigateToBeforePage = () => {
-        navigate(-1)
-    }
-    
     return(
         <>
             <TeacherHeader />
@@ -21,11 +16,6 @@ export default function TeacherErrorPage() {
                     <img src={daramgisad} alt="daramgisad" />
                     <p className="text-3xl">존재하지 않는 페이지 입니다.</p>
                     <div className="mt-4 space-y-2">
-                        <div className="block">
-                            <div className="inline-block text-center border-[2px] border-[#7C7C7C] bg-[#E3EEFF] px-3 py-1 font-bold rounded-[10px] hover:bg-[#D4DDEA]">
-                                <button onClick={navigateToBeforePage}>이전 페이지로 돌아가기</button>
-                            </div>
-                        </div>
                         <div className="block">
                             <div className="inline-block text-center border-[2px] border-[#7C7C7C] bg-[#E3EEFF] px-3 py-1 font-bold rounded-[10px] hover:bg-[#D4DDEA]">
                                 <button onClick={navigateToHomePage}>첫 화면으로 돌아가기</button>
