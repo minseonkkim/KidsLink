009f383df92c4c94cb985adb7aed496397536313
surina125 <jhyunsoo1125@gmail.com>
2024-08-13 21:41:21 +0900

    fix: 프로필 이미지 렌더링 추가
    - 알림장, 성장일지: 선생님 프로필 이미지 렌더링
    - 문서 제출: 아이 프로필 이미지 렌더링

--- frontend/src/pages/parent/ParentNoticeDetail.tsx
+++ frontend/src/pages/parent/ParentNoticeDetail.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import profileImg from "../../assets/parent/notice-daramgi.png"
 import { getNoticeDetail } from "../../api/notice"
 import { NoticeDetail } from "../../api/notice"
 import TeacherProfile from "../../components/parent/common/TeacherProfile"
@@ -44,30 +43,20 @@ export default function ParentNoticeDetail() {
     )
   }
 
-  const formatDate = (dateString: string) => {
-    const options: Intl.DateTimeFormatOptions = {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-    }
-    return new Date(dateString).toLocaleDateString('ko-KR', options);
-  }
 
   return (
     <>
       <div className="flex flex-col px-6">
-        <TeacherProfile profileImg={profileImg} teacherName={notice.teacherName} />
+        <TeacherProfile />
 
         <div className="relative w-full bg-[#fff9d7] rounded-[20px] mt-8 px-6 py-8 shadow-lg border-2 border-[#ffec8a]">
           {/* 테이프 효과 */}
           <div className="absolute -top-4 left-1/2 transform -translate-x-1/2 w-16 h-8 bg-yellow-300 z-10"></div>
 
-          <p className="text-[20px] font-bold text-[#212121] mb-2 text-center">
+          <p className="text-[20px] font-bold text-[#212121] mb-6 text-center">
             {notice.title}
           </p>
-          <p className="text-[14px] font-light text-[#353c4e] mb-6 text-center">
-            {formatDate(notice.noticeBoardDate)}
-          </p>
+
           <div className="text-[16px] text-[#212121] space-y-4 whitespace-pre-line">
             {notice.content}
           </div>
