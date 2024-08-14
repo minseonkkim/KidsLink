2c8b6e449c4a58421b320ce81fad3ea38c073965
sangmin0806 <cjstkd2866@naver.com>
2024-08-14 15:39:37 +0900

    fix: 상담예약일정 ê알고리즘 수ì 정

--- backend/src/main/java/com/ssafy/kidslink/common/service/VideoService.java
+++ backend/src/main/java/com/ssafy/kidslink/common/service/VideoService.java
@@ -9,8 +9,6 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
